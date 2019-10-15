781cba32c280108672004f93cd2f50f17a57ca51
Grandvaux <paul.grandvaux@outlook.fr>
2019-10-15 12:09:14 +0200

    TP Article Noté

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
@@ -30,4 +31,57 @@ class ArticleController extends AbstractController
             'article' => $article
         ]);
     }
+
+    /**
+     * @Route("/tableau", name="tableau_article")
+     */
+    public function tableauArticles(ArticleRepository $repo)
+    {
+        //Prendre l'ensemble des articles sous forme de tableau
+        $articles=$repo->findAll();
+        return $this->render('article/tableau.html.twig', [
+            'articles' => $articles //on va le donner dans le twig
+        ]);
+    }
+
+    /**
+     * Permet de créer un article
+     * 
+     * @Route("/articles/new", name="articles_create")
+     * 
+     * @return Response
+     */
+    public function create(){
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                    ->add('libelle')
+                    ->add('prix')
+                    ->add('description')
+                    ->add('save', SubmitType::class, [
+                        'label' => 'Créer ',
+                        'attr' => [
+                            'class' => 'btn btn-primary'
+                        ]
+                    ])
+                    ->getForm();
+
+        return $this->render('article/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * Permet d'afficher une seule annonce  
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show(Article $article){
+        return $this->render('article/show.html.twig', [
+          'article' => $article
+      ]);
+  }
+    
 }
